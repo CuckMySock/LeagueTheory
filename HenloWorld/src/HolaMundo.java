a983abf82b711e5ee8f4d01d9f95829c078a0c54
Aidan K <Aidan K>
2017-02-13 22:23:41 -0500

    test'

--- HenloWorld/src/HolaMundo.java
+++ HenloWorld/src/HolaMundo.java
@@ -1,11 +1,8 @@
 
 public class HolaMundo {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 			System.out.println("Henlo Woorld!");
 			System.out.println("Helllo you stinky World!");
 			System.out.println("Go eat a meteorite, ugly!");
 	}
-
 }
